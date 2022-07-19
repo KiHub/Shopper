ef9f503e91204fbc72b05c4e0a4f4f6692e57e8e
Kirill Ki <kirillkalapov@gmail.com>
2022-07-19 14:12:55 +0200

    Initial Commit

---
+++ Shopper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shopper
+//
+//  Created by  Mr.Ki on 19.07.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
